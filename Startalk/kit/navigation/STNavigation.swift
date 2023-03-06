750490fb6a490b9772c5512c4e4436924f37f4d1
lei <lei@MacBook.local>
2023-03-06 21:53:45 +0800

    navigation: add navigation decoding

--- Startalk/kit/navigation/STNavigation.swift
+++ Startalk/kit/navigation/STNavigation.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct STNavigation: Codable{
+struct STNavigation{
     var domain: String
     var host: String
     var port: Int
@@ -19,16 +19,34 @@ struct STNavigation: Codable{
         self.port = port
         self.apiUrl = apiUrl
     }
+        
+    static let `default` = STNavigation(
+        domain: "startalk.im",
+        host: "i.startalk.im",
+        port: 5202,
+        apiUrl: "https://i.startalk.im/newapi"
+    )
+}
+
+extension STNavigation: Codable{
     
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.domain = try container.decode(String.self, forKey: .domain)
-        self.host = try container.decode(String.self, forKey: .host)
-        self.port = try container.decode(Int.self, forKey: .port)
-        self.apiUrl = try container.decode(String.self, forKey: .apiUrl)
+    enum DecodingKeys: CodingKey{
+        case baseaddess
     }
     
-        
-    static let `default` = STNavigation(domain: "startalk.im", host: "i.startalk.im", port: 5202, apiUrl: "https://i.startalk.im/newapi")
-
+    enum BaseAddressKeys: CodingKey{
+        case domain
+        case xmpp
+        case protobufPort
+        case httpurl
+    }
+    
+    init(from decoder: Decoder) throws {
+        let rawContainer = try decoder.container(keyedBy: DecodingKeys.self)
+        let container = try rawContainer.nestedContainer(keyedBy: BaseAddressKeys.self, forKey: .baseaddess)
+        self.domain = try container.decode(String.self, forKey: .domain)
+        self.host = try container.decode(String.self, forKey: .xmpp)
+        self.port = try container.decode(Int.self, forKey: .protobufPort)
+        self.apiUrl = try container.decode(String.self, forKey: .httpurl)
+    }
 }
