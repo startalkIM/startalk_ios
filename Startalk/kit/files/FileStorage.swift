a4cb46c1f3bf560ed095d25f88ee1fb8a5e944cf
lei <lei@MacBook.local>
2023-03-11 19:04:51 +0800

    files: add cacher and storage, to be implemented

---
+++ Startalk/kit/files/FileStorage.swift
@@ -0,0 +1,20 @@
+//
+//  FileStorage.swift
+//  Startalk
+//
+//  Created by lei on 2023/3/11.
+//
+
+import Foundation
+
+//TODO: implement storage functions
+class FileStorage{
+    
+    func store(_ data: Data) -> String{
+        return ""
+    }
+    
+    func fetch(_ name: String) -> Data?{
+        return nil
+    }
+}
