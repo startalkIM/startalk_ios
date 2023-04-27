64568845a1f96d8cf39594cae075317e641a1e73
lei <lei@MacBook.local>
2023-04-27 20:19:46 +0800

    main controller： change  controller showing way to prevent advacning view loading

--- Startalk/views/STMainVController.swift
+++ Startalk/views/STMainVController.swift
@@ -10,40 +10,30 @@ import Combine
 
 class STMainVController: STSwitchController{
     
-    let loginController: STLoginVController
-    
-    let contentController: UINavigationController
-    
     let appStateManager = STKit.shared.appStateManager
     let notificationCenter = STKit.shared.notificationCenter
     
-    required init?(coder: NSCoder) {
-        loginController = STLoginVController()
-    
-        let tabController = STContentVController()
-        contentController = UINavigationController(rootViewController: tabController)
-        
-        super.init(coder: coder)
-        
-    }
-    
     override func viewDidLoad() {
-        if appStateManager.state == .login{
-            show(loginController)
-        }else{
-            show(contentController)
-        }
+        let state = appStateManager.state
+        setController(state: state)
 
         notificationCenter.observeAppStateChanged(self, handler: stateChanged(_:))
     }
     
     func stateChanged(_ state: STAppState){
         DispatchQueue.main.async { [self] in
-            if state == .login{
-                show(loginController)
-            }else{
-                show(contentController)
-            }
+            setController(state: state)
         }
+    }
+    
+    func setController(state: STAppState){
+        let controller: UIViewController
+        if state == .login{
+            controller = STLoginVController()
+        }else{
+            let tabController = STContentVController()
+            controller = UINavigationController(rootViewController: tabController)
+        }
+        show(controller)
     }
 }
